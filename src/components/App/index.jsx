7ec6415ae367fee48d0242fc04ad8549f2d6476f
Veselin <veselin.lk.petrov@gmail.com>
2019-02-15 16:28:11 +0200

    added some global styling

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,13 +5,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import NotFound from '../NotFound'
 import VideoPlayer from '../VideoPlayer'
 
+import GlobalStyle from "../../styles/GlobalStyle"
+
 const App = () => (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={VideoPlayer} />
-            <Route path="/:activeVideo" exact component={VideoPlayer} />
-            <NotFound />
-        </Switch>
+        <>
+            <Switch>
+                <Route path="/" exact component={VideoPlayer} />
+                <Route path="/:activeVideo" exact component={VideoPlayer} />
+                <NotFound />
+            </Switch>
+            <GlobalStyle />
+        </>
     </BrowserRouter>
 )
 
