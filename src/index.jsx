55677b1f5f0f090881ae6fe325228ef016af9417
Veselin <veselin.lk.petrov@gmail.com>
2019-02-21 00:04:59 +0200

    refactored src/index.jsx file

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
-// import * as serviceWorker from './serviceWorker'; // TODO:
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// serviceWorker.unregister(); TODO:
