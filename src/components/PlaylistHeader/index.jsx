936ccfab791d00701ed8810b88e76395363d4216
Veselin <veselin.lk.petrov@gmail.com>
2019-02-20 23:27:17 +0200

    refactored PlaylistHeader component

--- src/components/PlaylistHeader/index.jsx
+++ src/components/PlaylistHeader/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Style from './Style'
 import StyleJourney from './StyleJourney'
 
-const PlaylistItem = props => (
+const PlaylistHeader = props => (
     <Style>
-        {/* TODO: */}
-        <p>{props.active && props.active.title}</p>
+        <p>{props.active.title}</p>
         <StyleJourney>
-            {props.active && props.active.num} / {props.total}
+            {props.active.num} / {props.total}
         </StyleJourney>
     </Style>
 )
 
-export default PlaylistItem
+PlaylistHeader.propTypes = {
+    active: PropTypes.object.isRequired,
+    total: PropTypes.number.isRequired,
+}
+
+export default PlaylistHeader
